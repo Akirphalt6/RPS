d2629b2976d22294a4a2888d879769b38e131138
Akirphalt <77574895+Akirphalt6@users.noreply.github.com>
2022-03-25 16:26:12 +0900

    Test

---
+++ Test.java
@@ -0,0 +1,10 @@
+package RPS;
+import RPS.Controller.GameController;
+
+class Test{
+       public static void main(String[] args){
+
+       GameController game = new GameController();
+       game.start();
+    }
+}
